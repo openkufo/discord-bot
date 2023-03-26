d050614d14050e98cc9a9b4a5d215495a77a6bc0
paul Han <openkufo@gmail.com>
2023-03-26 22:07:28 +0900

    practice

--- app/src/main/java/com/paulhan/discord/App.java
+++ app/src/main/java/com/paulhan/discord/App.java
@@ -5,23 +5,33 @@ package com.paulhan.discord;
 
 import javax.security.auth.login.LoginException;
 
+import com.paulhan.discord.config.PropertiesReader;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class App extends ListenerAdapter {
 
-    public static void main(String[] args) throws LoginException
+    public static void main(String[] args) throws LoginException, InterruptedException
     {
-        JDA jda = JDABuilder.createDefault("token").build();
+        JDA jda = JDABuilder.createDefault(PropertiesReader.getProperty("DISCORD_TOKEN"))
+                            .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                            // .enableIntents(GatewayIntent.GUILD_MESSAGES)
+                            // .setStatus(OnlineStatus.ONLINE)
+                            // .disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
+                            // .setBulkDeleteSplittingEnabled(false)
+                            
+                            .setActivity(Activity.playing("실험중"))
+                            .build();
                 // .enableIntents(GatewayIntent.MESSAGE_CONTENT) // enables explicit access to message.getContentDisplay()
                 
-        //You can also add event listeners to the already built JDA instance
-        // Note that some events may not be received if the listener is added after calling build()
-        // This includes events such as the ReadyEvent
         jda.addEventListener(new App());
+        // jda.awaitReady();
     }
 
     @Override
@@ -35,7 +45,7 @@ public class App extends ListenerAdapter {
         else
         {
             System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
-                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                        event.getChannel().getName(), event.getMember().getEffectiveName(),
                         event.getMessage().getContentDisplay());
         }
     }
---
+++ app/src/main/java/com/paulhan/discord/config/PropertiesReader.java
@@ -0,0 +1,54 @@
+package com.paulhan.discord.config;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PropertiesReader {
+    /**
+     * Constant LOGGER.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesReader.class);
+
+    /**
+     * Constant PROPERTIES.
+     */
+    private static final Properties PROPERTIES;
+
+    /**
+     * Constant PROP_FILE.
+     */
+    private static final String PROP_FILE = "apikey.properties";
+
+    /**
+     * Default private constructor PropertiesReader.
+     */
+    private PropertiesReader() {
+    }
+
+    static {
+        PROPERTIES = new Properties();
+        final URL props = ClassLoader.getSystemResource(PROP_FILE);
+        try {
+            PROPERTIES.load(props.openStream());
+        } catch (IOException ex) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(ex.getClass().getName() + "PropertiesReader method");
+            }
+        }
+    }
+
+    /**
+     * Method getProperty.
+     *
+     * @param name String name file.
+     * @return Return property
+     */
+    public static String getProperty(final String name) {
+
+        return PROPERTIES.getProperty(name);
+    }
+}
