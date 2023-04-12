1b53327b5029017af81c2ecb93a5b91c37ff269a
paul Han <openkufo@gmail.com>
2023-04-12 15:51:52 +0900

    gpt 명령어 구조 추가

--- app/src/main/java/com/paulhan/discord/commands/ChatGptAdapter.java
+++ app/src/main/java/com/paulhan/discord/commands/ChatGptAdapter.java
@@ -8,18 +8,46 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ChatGptAdapter extends ListenerAdapter {
+    private final String GPT = "```GPT";
+    private final String CONTROL = "CONTROL";
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        
+        String user = event.getAuthor().getName();
+
         StringBuilder message = new StringBuilder(event.getMessage().getContentRaw());
-        boolean isCommand = message.substring(0, 6).equals("```gpt");
-        boolean isControlCommand = message.substring(0, 9).equals("```gptControl");
+
+
+        boolean isCommand;
+        
+        try{
+            isCommand = message.substring(0, 6).toUpperCase().equals(GPT);
+        } catch(StringIndexOutOfBoundsException e){
+            isCommand = false;
+        }
+
         if(!isCommand){
             return;
+        }
+        
+        
+        boolean isControlCommand;
+        try{
+            isControlCommand = message.substring(6, 13).toUpperCase().equals(CONTROL);
+        } catch(StringIndexOutOfBoundsException e){
+            isControlCommand = false;
         }
 
         message.setLength(0);
         message.append(event.getMessage().getContentStripped());
+        
+        boolean isOnwer = user.equals(App.OWNER);
+        if(isControlCommand && isOnwer){
+            if(message.toString().toUpperCase().equals("ALL")){
+                
+            }
+            return;
+        }
+        
         
         System.out.println("message length : " + message.length());
         System.out.println("gpt : " + message);
